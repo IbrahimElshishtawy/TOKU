a1bda49af04aacffb88f2afb8cb65254f0298ee1
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-05-07 18:06:35 +0300

    //

--- lib/screens/family_member_page.dart
+++ lib/screens/family_member_page.dart
@@ -7,16 +7,16 @@ class FamilyMemberPage extends StatelessWidget {
   FamilyMemberPage({super.key});
 
   final List<family_members> familyMembers = [
-    const family_members(englishmembers:'daughter' , japanesememebers:'kjdfbnvpd' , soundmembers:'assets/sounds/family_members/daughter.wav' , imagemembers: 'assets/images/family_members/family_daughter.png'),
-    const family_members(englishmembers:'son' , japanesememebers:'kjdfbnvpd' , soundmembers:'assets/sounds/family_members/son.wav' , imagemembers: 'assets/images/family_members/family_son.png'),
-    const family_members(englishmembers:'older brother' , japanesememebers:'kjdfbnvpd' , soundmembers:'assets/sounds/family_members/olderbrother.wav' , imagemembers: 'assets/images/family_members/family_older_brother.png'),
-    const family_members(englishmembers:'younger brother' , japanesememebers:'kjdfbnvpd' , soundmembers:'assets/sounds/family_members/youngerbrother.wav' , imagemembers: 'assets/images/family_members/family_younger_brother.png'),
-    const family_members(englishmembers:'older sister' , japanesememebers:'kjdfbnvpd' , soundmembers:'assets/sounds/family_members/oldersister.wav' , imagemembers: 'assets/images/family_members/family_older_sister.png'),
-    const family_members(englishmembers:'younger sister' , japanesememebers:'kjdfbnvpd' , soundmembers:'assets/sounds/family_members/youngersister.wav' , imagemembers: 'assets/images/family_members/family_younger_sister.png'),
-    const family_members(englishmembers:'grandmother' , japanesememebers:'kjdfbnvpd' , soundmembers:'assets/sounds/family_members/grandmother.wav' , imagemembers: 'assets/images/family_members/family_grandmother.png'),
-    const family_members(englishmembers:'grandfather' , japanesememebers:'kjdfbnvpd' , soundmembers:'assets/sounds/family_members/grandfather.wav' , imagemembers: 'assets/images/family_members/family_grandfather.png'),
-    const family_members(englishmembers:'younger brother' , japanesememebers:'kjdfbnvpd' , soundmembers:'assets/sounds/family_members/younger_brother.wav' , imagemembers: 'assets/images/family_members/family_younger_brother.png'),
-    const family_members(englishmembers:'younger sister' , japanesememebers:'kjdfbnvpd' , soundmembers:'assets/sounds/family_members/younger_sister.wav' , imagemembers: 'assets/images/family_members/family_younger_sister.png'),
+    const family_members(englishmembers:'daughter' , japanesememebers:'daughter' , soundmembers:'assets/sounds/family_members/daughter.wav' , imagemembers: 'assets/images/family_members/family_daughter.png'),
+    const family_members(englishmembers:'son' , japanesememebers:'son' , soundmembers:'assets/sounds/family_members/son.wav' , imagemembers: 'assets/images/family_members/family_father.png'),
+    const family_members(englishmembers:'older brother' , japanesememebers:'older' , soundmembers:'assets/sounds/family_members/olderbrother.wav' , imagemembers: 'assets/images/family_members/family_grandfather.png'),
+    const family_members(englishmembers:'younger brother' , japanesememebers:'younger brother' , soundmembers:'assets/sounds/family_members/youngerbrother.wav' , imagemembers: 'assets/images/family_members/family_grandmother.png'),
+    const family_members(englishmembers:'older sister' , japanesememebers:'older sister' , soundmembers:'assets/sounds/family_members/oldersister.wav' , imagemembers: 'assets/images/family_members/family_mother.png'),
+    const family_members(englishmembers:'younger sister' , japanesememebers:'younger sister' , soundmembers:'assets/sounds/family_members/youngersister.wav' , imagemembers: 'assets/images/family_members/family_older_sister.png'),
+    const family_members(englishmembers:'grandmother' , japanesememebers:'grandmother' , soundmembers:'assets/sounds/family_members/grandmother.wav' , imagemembers: 'assets/images/family_members/family_older_brother.png'),
+    const family_members(englishmembers:'grandfather' , japanesememebers:'grandfather' , soundmembers:'assets/sounds/family_members/grandfather.wav' , imagemembers: 'assets/images/family_members/family_son.png'),
+    const family_members(englishmembers:'younger brother' , japanesememebers:'younger brother' , soundmembers:'assets/sounds/family_members/younger_brother.wav' , imagemembers: 'assets/images/family_members/family_younger_brother.png'),
+    const family_members(englishmembers:'younger sister' , japanesememebers:'kj' , soundmembers:'assets/sounds/family_members/younger_sister.wav' , imagemembers: 'assets/images/family_members/family_younger_sister.png'),
   ];
 
   @override
